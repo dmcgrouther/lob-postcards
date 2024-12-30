8ecb137751a743a223daba1e6d90373ff664142b
dmcgrouther <dbmcgrouther@gmail.com>
2024-12-30 13:55:53 -0800

    removed unused html template

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 #import get_address and postcard templates
 from get_address import get_address
-from templates import html_front_template_1, html_front_template_2, html_front_template_3, html_back_template
+from templates import html_front_template_1, html_front_template_2, html_back_template
 
 # Load the environment variables from the .env file
 load_dotenv()
