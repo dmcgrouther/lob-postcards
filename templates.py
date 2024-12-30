09cfa432bf14073bc0a11a328a75e1c7f393d358
dmcgrouther <dbmcgrouther@gmail.com>
2024-12-30 13:36:39 -0800

    changed templates around

--- templates.py
+++ templates.py
@@ -1,5 +1,9 @@
 # Example HTML template for the front of the postcard with a placeholder for the name
-html_front_template = """
+html_front_template_1 = "https://cdn.prod.website-files.com/5e1e5c62fa3d44c96b4170a1/5eb8636a79f886f204485913_gtmpl_44f242991d48f5_4x6_front.pdf"
+
+html_front_template_2 = "https://cdn.prod.website-files.com/5e1e5c62fa3d44c96b4170a1/613ba39c6c92493d37b94bc4_4x6%20Retail%20front.pdf"
+
+html_front_template_3 = """
 <html>
   <body>
     <h1>Hello {{name}}!</h1>
